c98d9698eaef654894a22467350551fd778137f5
Farzad Azizi Zade <84402243+FarzadAziziZade@users.noreply.github.com>
2021-08-18 14:18:13 +0430

    Add files via upload

---
+++ ESP8266TEST/ESP8266TEST.ino
@@ -0,0 +1,35 @@
+// original soarce: https://create.arduino.cc/projecthub/Niv_the_anonymous/esp8266-beginner-tutorial-project-6414c8
+
+#define BLYNK_PRINT Serial
+
+
+#include <ESP8266WiFi.h>
+#include <BlynkSimpleEsp8266.h>
+
+// You should get Auth Token in the Blynk App.
+// Go to the Project Settings (nut icon).
+char auth[] = "your auth token code from blynk app";
+
+// Your WiFi credentials.
+// Set password to "" for open networks.
+char ssid[] = "your wifi name";
+char pass[] = "your wifi password";
+
+void setup()
+{
+  // Debug console
+  Serial.begin(9600);
+
+  Blynk.begin(auth, ssid, pass);
+  // You can also specify server:
+  //Blynk.begin(auth, ssid, pass, "blynk-cloud.com", 80);
+  //Blynk.begin(auth, ssid, pass, IPAddress(192,168,1,100), 8080);
+}
+
+void loop()
+{
+  Blynk.run();
+  // You can inject your own code or combine it with other sketches.
+  // Check other examples on how to communicate with Blynk. Remember
+  // to avoid delay() function!
+}
